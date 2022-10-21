d86fd15ee32b21174abef884c3b85113dc2aa581
Noel Uba <ubanoel2000@gmail.com>
2022-10-21 20:58:43 +0100

    task: Gapminder chart

--- 05/5.10.0/js/main.js
+++ 05/5.10.0/js/main.js
@@ -1,9 +1,117 @@
 /*
-*    main.js
-*    Mastering Data Visualization with D3.js
-*    Project 2 - Gapminder Clone
-*/
+ *    main.js
+ *    Mastering Data Visualization with D3.js
+ *    Project 2 - Gapminder Clone
+ */
 
-d3.json("data/data.json").then(function(data){
-	console.log(data);
-})
+const MARGIN = { LEFT: 100, RIGHT: 10, TOP: 10, BOTTOM: 100 };
+const WIDTH = 800 - MARGIN.LEFT - MARGIN.RIGHT;
+const HEIGHT = 500 - MARGIN.TOP - MARGIN.BOTTOM;
+
+const svg = d3
+  .select("#chart-area")
+  .append("svg")
+  .attr("width", WIDTH + MARGIN.LEFT + MARGIN.RIGHT)
+  .attr("height", HEIGHT + MARGIN.TOP + MARGIN.BOTTOM);
+
+const g = svg
+  .append("g")
+  .attr("transform", `translate(${MARGIN.LEFT}, ${MARGIN.TOP})`);
+
+let time = 0;
+
+// SCALES
+const x = d3.scaleLog().base(10).range([0, WIDTH]).domain([142, 150000]);
+const y = d3.scaleLinear().range([HEIGHT, 0]).domain([0, 90]);
+const area = d3
+  .scaleLinear()
+  .range([25 * Math.PI, 1500 * Math.PI])
+  .domain([2000, 1400000000]);
+const continentColor = d3.scaleOrdinal(d3.schemePastel1);
+
+// LABELS
+const xLabel = g
+  .append("text")
+  .attr("y", HEIGHT + 50)
+  .attr("x", WIDTH / 2)
+  .attr("font-size", "20px")
+  .attr("text-anchor", "middle")
+  .text("GDP-per-capita");
+const yLabel = g
+  .append("text")
+  .attr("transform", "rotate(-90)")
+  .attr("y", -40)
+  .attr("x", -170)
+  .attr("font-size", "20px")
+  .attr("text-anchor", "middle")
+  .text("life expectancy");
+const yearLabel = g
+  .append("text")
+  .attr("y", HEIGHT - 10)
+  .attr("x", WIDTH - 40)
+  .attr("font-size", "40px")
+  .attr("opacity", "0.4")
+  .attr("text-anchor", "middle")
+  .text("1800");
+
+// X AXIS
+const xAxisCall = d3
+  .axisBottom(x)
+  .tickValues([400, 4000, 40000])
+  .tickFormat(d3.format("$"));
+g.append("g")
+  .attr("class", "x axis")
+  .attr("transform", `translate(0, ${HEIGHT})`)
+  .call(xAxisCall);
+
+// Y AXIS
+const yAxisCall = d3.axisLeft(y);
+g.append("g").attr("class", "y axis").call(yAxisCall);
+
+d3.json("data/data.json").then(function (data) {
+  console.log(data);
+  const formatedData = data.map((year) => {
+    return year.countries
+      .filter((country) => country.income && country.life_exp)
+      .map((country) => {
+        country.income = Number(country.income);
+        country.life_exp = Number(country.life_exp);
+        return country;
+      });
+  });
+
+  // update graph every 100milliseconds
+  d3.interval(function () {
+    // start from the beginning if finished
+    time = time < 214 ? time + 1 : 0;
+    update(formatedData[time]);
+  }, 100);
+
+  // run first time for visualization
+  update(formatedData[0]);
+});
+
+function update(data) {
+  // transition time for the graph
+  const t = d3.transition().duration(100);
+
+  // JOIN new data with old elements
+  const circles = g.selectAll("circle").data(data, (d) => d.country);
+
+  // EXIT old elements not present in new data
+  circles.exit().remove();
+
+  // ENTER new elements present in new data
+  circles
+    .enter()
+    .append("circle")
+    .attr("fill", (d) => continentColor(d.continent))
+    .merge(circles)
+    .transition(t)
+    .attr("cy", (d) => y(d.life_exp))
+    .attr("cx", (d) => x(d.income))
+    .attr("r", (d) => Math.sqrt(area(d.population) / Math.PI));
+
+  // update time label
+  yearLabel.text(String(time + 1800));
+}
